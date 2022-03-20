d22b2edc047058cc897b798bf4a0edf43316243a
Karenkoech <karenkoech3@gmail.com>
2022-03-20 19:46:10 -0400

    add all changes made to the file

--- script.js
+++ script.js
@@ -1,10 +1,95 @@
-document.getElementById('shippingMethod').addEventListener('submit', actions);
-function actions(event){
-  event.preventDefault();
-  var method = document.getElementById('shippingMethod').querySelectorAll('input[name="r_method"]');
-  for (var i = 0; i < method.length; i++){
-  	if(method[i].checked == true){
-    	alert(method[i].value)
+$(document).ready(function() {
+    $(".result").hide()
+    $(".purchase").click(function() {
+        var size = $("#size  option:selected").val();
+        var crust = $("#crust  option:selected").val();
+        var toppings = $("#toppings  option:selected").val()
+        var quantity = $(".quantity").val();
+        var location = $("#location option:selected").val()
+        var price = $("input[type='checkbox']").val();
+
+        function quantityChanged() {
+
+            if (isNaN(quantity) || quantity <= 0) {
+                quantity = 1
+            }
+        }
+        quantityChanged();
+        var crustPrice;
+        if (toppings === 'mushroom') {
+            crustPrice = 50
+        } else if (toppings === 'Bacon') {
+            crustPrice = 150
+        } else if (toppings === 'Extra Cheese') {
+
+            crustPrice = 100;
+        }
+        var toppingPrice;
+        if (crust === 'thick') {
+            toppingPrice = 100
+        } else if (crust === 'crispy') {
+            toppingPrice = 120
+        } else if (crust === 'gluttenFree') {
+            toppingPrice = 150
+        }
+        var price;
+        if (size === 'large') {
+            price = 1600
+        } else if (size === 'medium') {
+            price = 1200
+        } else if (size === 'small') {
+            price = 600
+        } else if (size === 'You') {
+            alert("Please select your size!")
+            price = 0;
+        }
+        
+
+        var totalCost = quantity * (price + crustPrice + toppingPrice);
+        var overalCost = parseInt(totalCost);
+
+        $(".cart").append(
+            `
+                            <tr>
+                                <td>${size}</td> 
+                                <td>${crust}</td>
+                                <td>${toppings}</td>
+                                <td>${quantity}</td>
+                                <td>${location}</td>
+                                <td>${overalCost}</td>
+                                <td><button class="btn remove">remove</button></td>
+                            </tr>
+                            `
+        )
+
+
+    })
+
+    var value;
+    var theTotal = 0;
+
+    $(".checkout").click(function() {
+        $("td:nth-child(6)").each(function() {
+            value = $(this).html();
+
+            theTotal += parseInt(value);
+            $(".result").text(' Thank you for shopping with us. ' + ' Your payable order is: ' + theTotal + `.`).show();
+        });
+
+    })
+    var deletebtn = $(".remove");
+    deletebtn.addClass('Remove')
+    deletebtn.click(function(e) {
+        const btn = e.target;
+        btn.closest('tr').remove();
+    })
+
+})
+$('input[type="checkbox"]').click(function() {
+    if ($(this).prop("checked") == true) {
+        $(".location").show()
+    } else if ($(this).prop("checked") == false) {
+        $(".location").hide()
     }
-  }
-}
+});
+
